ed5a45a89bf0b267c61b12be663b650225dacb23
razor <acm09@yandex.ru>
2026-06-14 15:14:07 +0300

    update

---
+++ readme.md
@@ -0,0 +1,23 @@
+### Установка и запуск приложения на моках
+
+```
+npm run fetch
+
+npm run mocks:dev
+```
+
+### Локальный запуск сборки
+
+```
+npm run build
+
+npm run serve:prod
+```
+
+### Кодогенерация
+
+```
+npm run generate component First Second
+
+npm run generate mocks first second
+```
---
+++ tools/codegen/generate.ts
@@ -0,0 +1,62 @@
+import { info } from 'console'
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs'
+import { resolve } from 'path'
+import { argv } from 'process'
+import { BoilerplateFile, ProcessArgs } from './typings'
+
+const boilerplate = resolve('tools/boilerplate')
+const pages = resolve('src/components/pages')
+const ui = resolve('src/components/ui')
+const kit = resolve('src/components/kit')
+const icons = resolve('src/components/icons')
+const handlers = resolve('src/mocks/handlers')
+
+const insertContent = (boilerplateFile: BoilerplateFile, arg: string): string => {
+  const search = new RegExp('__arg__', 'g')
+  return readFileSync(`${boilerplate}/${boilerplateFile}.plain`, 'utf-8').replace(search, arg)
+}
+
+const isComponentExists = (arg: string): boolean => {
+  return existsSync(`${pages}/${arg}`) || existsSync(`${ui}/${arg}`) || existsSync(`${kit}/${arg}`) || existsSync(`${icons}/${arg}`)
+}
+
+const isMocksExists = (arg: string): boolean => {
+  return existsSync(`${handlers}/${arg}`)
+}
+
+const createComponent = (arg: string): void => {
+  if (isComponentExists(arg)) {
+    info(`${arg} already exists`)
+  } else {
+    const folder = `${pages}/${arg}`
+    mkdirSync(folder)
+    writeFileSync(`${folder}/index.tsx`, insertContent('component', arg))
+    writeFileSync(`${folder}/typings.d.ts`, insertContent('typings', arg))
+    writeFileSync(`${folder}/style.scss`, insertContent('style', arg))
+  }
+}
+
+const createMocks = (arg: string): void => {
+  if (isMocksExists(arg)) {
+    info(`${arg} already exists`)
+  } else {
+    const folder = `${handlers}/${arg}`
+    mkdirSync(folder)
+    writeFileSync(`${folder}/index.ts`, insertContent('handlers', arg))
+    writeFileSync(`${folder}/mocks.ts`, insertContent('mocks', arg))
+  }
+}
+
+const generate = (): void => {
+  const [typeOfCodegen, ...args] = argv.slice(2) as ProcessArgs
+  args.forEach((arg) => {
+    switch (typeOfCodegen) {
+      case 'component':
+        return createComponent(arg)
+      case 'mocks':
+        return createMocks(arg)
+    }
+  })
+}
+
+generate()
